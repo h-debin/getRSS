bca9c300b2b6e8df6f9cfa113603a2a89a609791
Minghe Huang <h.minghe@gmail.com>
2015-03-19 18:24:02 +0800

    cleanup

--- readability/test.rb
+++
@@ -1,4 +0,0 @@
-puts Dir.pwd
-
-puts File.dirname(__FILE__)
-puts File.absolute_path(__FILE__)
--- readability/test_readability.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'readability'
-
-describe Readability do
-  it "should be able to get content of article" do
-    expect(Readability.content_of_article(url:"http://news.163.com/15/0313/17/AKJR7NAE00014SEH.html")).to eq('')
-  end
-end
