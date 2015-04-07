af90d79457a4c536ed22065c54b630b83cba1e48
Minghe Huang <h.minghe@gmail.com>
2015-04-07 14:43:40 +0800

    enhance the article image getting algorithm

--- lib/image_getter.rb
+++ lib/image_getter.rb
@@ -5,12 +5,16 @@ class ImageGetter
 
   def self.on(url:)
     images = []
-    doc = Nokogiri::HTML(open(url))
-    doc.css("img").each do |img|
-      images << img.attr("src")
-    end
-    doc.css("IMG").each do |img|
-      images << img.attr("src")
+    begin
+      doc = Nokogiri::HTML(open(url))
+      doc.css("img").each do |img|
+        images << img.attr("src")
+      end
+      doc.css("IMG").each do |img|
+        images << img.attr("src")
+      end
+    rescue Exception => e
+      puts "Exception: #{e.to_s}"
     end
     images
   end
@@ -22,10 +26,28 @@ class ImageGetter
   def self.largest(url:)
     images = on(url:url)
     max_size = 0
-    max_image = images[0]
+    max_image = nil;
     images.each do |img|
       size = FastImage.size(img)
       if size != nil && size.length == 2
+
+        # ++
+        # when a image's height (or width) is way large than width (height)
+        # it should not be the feature image of a article 
+        # ++
+        rate = size[0] * 1.0 / size[1]
+        if rate > 3 || rate < 0.333
+          next
+        end
+
+        # ++
+        # when the height or width of image is two small
+        # it should not be the feature image of a article also
+        # ++
+        if size[0] < 20 || size[1] < 20
+          next
+        end
+
         size = size[0] * size[1]
         if size > max_size
           max_size = size
