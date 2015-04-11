a77bc518dce71f86301a499d6729698b7544e861
Minghe Huang <h.minghe@gmail.com>
2015-04-11 20:26:46 +0800

    let getRSS and Rails share a same database

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,12 @@ require 'redis'
 require 'date'
 
 APP_ROOT = File.absolute_path("../../", __FILE__)
-DATABASE_ROOT = File.join(APP_ROOT, "db")
+
+# ++
+# you can set any path let database lie in
+# ++
+RAILS_DB_DIR = "/Users/huangmh/Codes/iOSonRails/Rails/"
+DATABASE_ROOT = File.join(RAILS_DB_DIR, "db")
 BIN_ROOT = File.join(APP_ROOT, "bin")
 LIB_ROOT = File.join(APP_ROOT, "lib")
 
@@ -11,11 +16,15 @@ Dir[LIB_ROOT + "/*"].each do |lib|
   require lib
 end
 
+# ++
+# the default database is production db for Rails
+# you can override it as you need
+# ++
 if File.exists?(DATABASE_ROOT + "./news.db") 
   raise "Exception: no news.db exist, please rake to create it first"
 else
   ActiveRecord::Base.establish_connection(:adapter => "sqlite3", 
-                                          :database => "#{DATABASE_ROOT}/news.db",
+                                          :database => "#{DATABASE_ROOT}/production.sqlite3",
                                           :timeout => 10000)
   #if !ActiveRecord::Base.connection.table_exists?("news")
   #  raise "Exception: no news table in news.db, please rake to create it first"
